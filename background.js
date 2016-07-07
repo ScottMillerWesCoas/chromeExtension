0add5aef42118094f3b03a0424a462b1138e0758
Scott Miller <ScottMiller@Scotts-MacBook-Pro.local>
2016-07-07 01:32:25 -0700

    updated manifest JSON, html opens new page after onsubmit, background.js re-directs to search page

--- background.js
+++ background.js
@@ -1,19 +1,25 @@
-chrome.app.runtime.onLaunched.addListener(function(launchData) {
-  var windowWidth = 300;
-  var windowHeight = 325;
-  chrome.app.window.create('index.html', {
-    outerBounds: { // 'bounds' is deprecated, and you want full window size
-      width: windowWidth,
-      height: windowHeight,
-      left: screen.availWidth - windowWidth,
-      top: screen.availHeight - windowHeight,
-    },
-    resizable: true,
-    frame: 'none'
-  });      
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.create({ url: 'file:///Users/ScottMiller/Desktop/chromeExtension/index.html?search=sdfsdf' });
 });
 
-function myFunction() {
-    // var myWindow = window.open("http://www.editpad.org","", "width=400,height=400,top=400");
-    alert('testing'); 
-}
+// chrome.extension.runtime.onLaunched.addListener(function(launchData) {
+//   // var windowWidth = 300;
+//   // var windowHeight = 325;
+//   chrome.app.window.create(
+//     'index.html', 
+//     {
+//     outerBounds: { // 'bounds' is deprecated, and you want full window size
+//       width: 400,
+//       height: 400,
+//       // left: screen.availWidth - windowWidth,
+//       // top: screen.availHeight - windowHeight,
+//     },
+//     // resizable: true,
+//     // frame: 'none'
+//   });      
+// });
+
+// // function myFunction() {
+// //     // var myWindow = window.open("http://www.editpad.org","", "width=400,height=400,top=400");
+// //     alert('testing'); 
+// // }
---
+++ popup.js
@@ -0,0 +1,7 @@
+alert('whatever'); 
+
+function othername() {
+    input = document.getElementById("testing").value;
+    
+    var myWindow = window.open("http://www.editpad.org", "", "width=600,height=300,top=500");
+}
